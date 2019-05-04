375f424674e2eebae562b370f2de3532441e224d
Vesa Karvonen <vesa.a.j.k@gmail.com>
2019-05-04 18:41:45 +0300

    Use conventional doc comments

--- provides/include/mcs_v1/synopsis.hpp
+++ provides/include/mcs_v1/synopsis.hpp
@@ -6,27 +6,25 @@ namespace mcs_v1 {
 
 // lock.hpp ====================================================================
 
-// Non-recursive MCS spin lock.
+/// Non-recursive MCS spin lock.
 struct Lock {
+  /// Type of lock holder objects.
   struct holder_t;
-  // Type of lock holder objects.
 
+  /// Type of lock objects.
   struct lock_t;
-  // Type of lock objects.
 
+  /// Acquires the lock.  The `holder` must be allocated by the caller, kept
+  /// alive while the lock is held, and passed to `release` to release the lock.
+  /// A unique holder per live acquire is required.
   static void acquire(lock_t &lock, holder_t &holder);
-  // Acquires the lock.
-  //
-  // The `holder` must be allocated by the caller, kept alive while the lock is
-  // held, and passed to `release` to release the lock.  A unique holder per
-  // live acquire is required.
 
+  /// Releases the lock.
   static void release(lock_t &lock, holder_t &holder);
-  // Releases the lock.
 
+  /// Invokes the action holding the lock.
   template <class Action>
   static std::invoke_result_t<Action> holding(lock_t &lock, Action &&action);
-  // Invokes the action holding the lock.
 };
 
 } // namespace mcs_v1
